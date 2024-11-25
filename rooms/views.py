f47864428e3d69219bed162fb49c4182562c2140
Abdullahssg <bdallhalmhaqry926@gmail.com>
2024-11-26 01:39:38 +0300

    add views and urls and templates room

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def rooms_types(request):
+     
+     return render(request, 'room/rooms_types.html')
+
+
+def rooms_availability(request):
+     
+     return render(request, 'room/rooms_availability.html')
+
+
+def rooms_categories(request):
+     
+     return render(request, 'room/rooms_categories.html')
+
+
+def rooms_prices(request):
+     
+     return render(request, 'room/rooms_prices.html')
