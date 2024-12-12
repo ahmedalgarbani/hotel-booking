7b9b5d6a1713a82e78761ced1ec57acafb6da1da
Ammar777782439 <ammarragha@gmail.com>
2024-12-12 17:05:48 +0300

    add approve_hotel_request in admin2

--- hotels/urls.py
+++ hotels/urls.py
@@ -18,15 +18,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# ---------------------
+# مسارات تغير الغهه
+from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls',namespace='users')),
     path('HotelManagement/', include('HotelManagement.urls',namespace='HotelManagementz')),
     
-    
     path('rooms/', include('rooms.urls')),
     path('reviews/', include('reviews.urls')),
     path('services/', include('services.urls')),
+    # ----------------------
+    # مسار تغير الغه 
+    path('i18n/setlang/', set_language, name='set_language'),
 ]
 
 if settings.DEBUG:
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,8 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.hashers import make_password
 from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 from .models import CustomUser, HotelAccountRequest
 
 @admin.register(CustomUser)
@@ -10,17 +10,62 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['username', 'email', 'user_type', 'is_active']
     list_filter = ['user_type', 'is_active']
     search_fields = ['username', 'email']
+    
+    fieldsets = (
+        (_('معلومات الحساب'), {'fields': ('username', 'password')}),
+        (_('المعلومات الشخصية'), {'fields': ('first_name', 'last_name', 'email', 'phone')}),
+        (_('الصلاحيات'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_type', 'groups', 'user_permissions')}),
+        (_('تواريخ مهمة'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    def username_display(self, obj):
+        return obj.username
+    username_display.short_description = _('اسم المستخدم')
+
+    def email_display(self, obj):
+        return obj.email
+    email_display.short_description = _('البريد الإلكتروني')
+
+    def user_type_display(self, obj):
+        return obj.get_user_type_display()
+    user_type_display.short_description = _('نوع المستخدم')
+
+    def is_active_display(self, obj):
+        return obj.is_active
+    is_active_display.short_description = _('نشط')
+    is_active_display.boolean = True
+
+    list_display = ['username_display', 'email_display', 'user_type_display', 'is_active_display']
 
 @admin.register(HotelAccountRequest)
 class HotelAccountRequestAdmin(admin.ModelAdmin):
-    list_display = ['hotel_name', 'owner_name', 'email', 'status']
     list_filter = ['status']
-    actions = ['approve_hotel_request']
+    list_display_links = ['hotel_name_display']
+    search_fields = ['hotel_name', 'owner_name', 'email']
+    actions = ['approve_hotel_request', 'deactivate_hotel_request']
+
+    def hotel_name_display(self, obj):
+        return obj.hotel_name
+    hotel_name_display.short_description = _('اسم الفندق')
+
+    def owner_name_display(self, obj):
+        return obj.owner_name
+    owner_name_display.short_description = _('اسم المالك')
+
+    def email_display(self, obj):
+        return obj.email
+    email_display.short_description = _('البريد الإلكتروني')
+
+    def status_display(self, obj):
+        return obj.get_status_display()
+    status_display.short_description = _('الحالة')
+
+    list_display = ['hotel_name_display', 'owner_name_display', 'email_display', 'status_display']
 
     def approve_hotel_request(self, request, queryset):
         for hotel_request in queryset:
             try:
-                # إنشاء حساب مستخدم جديد
+               
                 user = CustomUser.objects.create(
                     username=hotel_request.hotel_name,
                     email=hotel_request.email,
@@ -31,7 +76,7 @@ class HotelAccountRequestAdmin(admin.ModelAdmin):
                     is_active=True
                 )
                 
-                # تحديث حالة الطلب إلى مقبول
+               
                 hotel_request.status = 'approved'
                 hotel_request.save()
                 
@@ -46,4 +91,39 @@ class HotelAccountRequestAdmin(admin.ModelAdmin):
                     f"خطأ في إنشاء حساب المستخدم للفندق {hotel_request.hotel_name}: {str(e)}",
                     level=messages.ERROR
                 )
-    approve_hotel_request.short_description = "الموافقة على طلبات الفنادق المحددة"
+    approve_hotel_request.short_description = _("الموافقة على طلبات الفنادق المحددة")
+
+
+
+
+    def deactivate_hotel_request(self, request, queryset):
+        for hotel_request in queryset:
+            try:
+                
+                user = CustomUser.objects.filter(
+                    username=hotel_request.hotel_name,
+                    email=hotel_request.email,
+                    user_type='hotel_manager'
+                ).first()
+                
+                if user:
+                    user.is_active = False
+                    user.save()
+                    
+               
+
+                hotel_request.status = 'cancelled'
+                hotel_request.save()
+                
+                self.message_user(
+                    request,
+                    f"تم إلغاء تفعيل حساب المستخدم للفندق: {hotel_request.hotel_name}",
+                    level=messages.SUCCESS
+                )
+            except Exception as e:
+                self.message_user(
+                    request,
+                    f"خطأ في إلغاء تفعيل حساب المستخدم للفندق {hotel_request.hotel_name}: {str(e)}",
+                    level=messages.ERROR
+                )
+    deactivate_hotel_request.short_description = _("إلغاء تفعيل الفنادق المحددة")
