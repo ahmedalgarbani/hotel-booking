c0529c38d179818f9c144bb2f9bc1b2cbab472cb
ahmed-algarbani <ahmedalgarbani776@gmail.com>
2025-01-17 11:51:22 +0300

    some project page

--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request,'frontend/home/index.html')
 
+def about(request):
+    return render(request,'frontend/home/pages/about.html')
+
