4e68c3c9c54dfbce3f19a7b31b073480deb557ba
MotasamAhmad <motasam735336099@gmail.com>
2025-01-20 22:03:53 +0300

    up date view hotel mangmnt to add hotel

--- HotelManagement/views.py
+++ HotelManagement/views.py
@@ -13,13 +13,18 @@ def add_hotel(request):
             # Handle multiple images
             images = request.FILES.getlist('image')
             if images:
+                hotel_name = hotel_request.official_name.replace(' ', '_')
+                folder_path = os.path.join(settings.MEDIA_ROOT, 'hotel_requests', hotel_name)
+                if not os.path.exists(folder_path):
+                    os.makedirs(folder_path)
+
                 image_paths = []
                 for image in images:
-                     file_path = os.path.join(settings.MEDIA_ROOT, 'hotel_requests', image.name)
-                     with open(file_path, 'wb+') as destination:
-                         for chunk in image.chunks():
-                             destination.write(chunk)
-                     image_paths.append(f'hotel_requests/{image.name}')
+                    file_path = os.path.join(folder_path, image.name)
+                    with open(file_path, 'wb+') as destination:
+                        for chunk in image.chunks():
+                            destination.write(chunk)
+                    image_paths.append(f'hotel_requests/{hotel_name}/{image.name}')
 
                 hotel_request.image = image_paths[0] if image_paths else None  # Save path of the first image
             hotel_request.save()
