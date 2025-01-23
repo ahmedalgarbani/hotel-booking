f22a58b8b044cb61fc20d15586cc6dad604f0b7d
ahmed-algarbani <ahmedalgarbani776@gmail.com>
2025-01-23 22:51:02 +0300

    non

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from HotelManagement.models import Hotel  # Import the model
+from HotelManagement.models import Hotel 
 
 
 
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
-        fields = '__all__'  # Or specify fields explicitly: ['id', 'name', 'description', 'price', 'created_at']
+        fields = '__all__'  
