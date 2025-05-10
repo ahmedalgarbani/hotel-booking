e94c98ba4a1c623b3acda4ba1b091b2bed7e5038
Ammar777782439 <ammarragha@gmail.com>
2025-05-10 22:18:56 +0300

    تعديل خق التمديد

--- bookings/admin_classes/extension_movement_admin.py
+++ bookings/admin_classes/extension_movement_admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
+from .mixins import HotelManagerAdminMixin
 
 
-class ExtensionMovementAdmin(admin.ModelAdmin):
+class ExtensionMovementAdmin(HotelManagerAdminMixin, admin.ModelAdmin):
     list_display = (
         'movement_number', 'booking', 'original_departure', 'new_departure',
         'extension_date', 'duration', 'reason', 'payment_button'
