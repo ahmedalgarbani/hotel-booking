75b1bc4fd751fc0780f14ec204640f4fe57a20ba
AmmarYasserRagha <93614263+Ammar777782439@users.noreply.github.com>
2025-05-12 04:45:13 +0300

    Update admin.py

--- services/admin.py
+++ services/admin.py
@@ -48,7 +48,7 @@ class CouponAdmin(AutoUserTrackMixin, admin.ModelAdmin):
     formatted_description.short_description = "الوصف"
     def get_readonly_fields(self, request, obj=None):
         if not request.user.is_superuser:
-            return ('created_at', 'updated_at', 'created_by', 'updated_by','hotel','deleted_at')
+            return ('created_at', 'updated_at', 'created_by', 'updated_by','deleted_at')
         return self.readonly_fields
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
